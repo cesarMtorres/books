48a3fff09a911e68deb3798cc7f7883e26ef6f83
cesar Molina <cesar.molina.torres.05@gmail.com>
2021-05-29 00:34:16 -0400

    vistas funcionales

--- books/views.py
+++ books/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+import json
 # Create your views here.
+booksData = open(
+    '/home/zer0x90/python/bookst/books.json').read()
+
+data = json.loads(booksData)
 
 
 def index(request):
-    return HttpResponse('helllo books')
+    context = {'books': data}
+    return render(request, 'books/index.html', context)
+ 
+
+def show(request, id):
+    single_book = list()
+    
+    for book in data:
+        if book['id'] == id:
+            single_book = book
+
+    context = {'book': single_book}
+    return render(request, 'books/show.html', context)
+ 
